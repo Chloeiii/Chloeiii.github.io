94103686ae28e5a553126e1b8bfbf1c8f40dca69
Chloe Yu <yuyaxi@uvic.ca>
2018-03-08 16:14:27 -0800

    remade navigation bar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $("li a").hover(
-        function(){$(this).css("background-color", "rgba(241, 215, 186, 0.2)");}, 
-        function(){$(this).css("background-color", "transparent");}
-    );
+    // $("li a").hover(
+    //     function(){$(this).css("background-color", "rgba(241, 215, 186, 0.4)");}, 
+    //     function(){$(this).css("background-color", "transparent");}
+    // );
     $("li a").click(
         function(){$(this).css("color", "#7d4627");}
     );
